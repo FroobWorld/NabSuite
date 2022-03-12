b0cb147409866276390917473ddc2fdc3356979c
froobynooby <froobynooby@froobworld.com>
2022-03-13 07:44:39 +0930

    Fix potential overflow

--- src/main/java/com/froobworld/nabsuite/modules/protect/util/PlayerCauser.java
+++ src/main/java/com/froobworld/nabsuite/modules/protect/util/PlayerCauser.java
@@ -3,28 +3,42 @@ package com.froobworld.nabsuite.modules.protect.util;
 import org.bukkit.entity.*;
 import org.bukkit.projectiles.ProjectileSource;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 public class PlayerCauser {
 
-    public static Player getPlayerCauser(Entity causer, boolean includeTargets) {
+    private static Player getPlayerCauser(Entity causer, boolean includeTargets, Set<UUID> visitedEntities) {
+        if (causer != null) {
+            if (visitedEntities.contains(causer.getUniqueId())) {
+                return null;
+            }
+            visitedEntities.add(causer.getUniqueId());
+        }
         if (causer instanceof Player) {
             return (Player) causer;
         }
         if (causer instanceof Projectile) {
             ProjectileSource source = ((Projectile) causer).getShooter();
             if (source instanceof Entity) {
-                return getPlayerCauser((Entity) source);
+                return getPlayerCauser((Entity) source, includeTargets, visitedEntities);
             }
         }
         if (includeTargets && causer instanceof Monster) {
-            return getPlayerCauser(((Monster) causer).getTarget());
+            return getPlayerCauser(((Monster) causer).getTarget(), includeTargets, visitedEntities);
         }
         if (causer instanceof Tameable) {
             AnimalTamer tamer = ((Tameable) causer).getOwner();
             if (tamer instanceof Entity) {
-                return getPlayerCauser((Entity) tamer);
+                return getPlayerCauser((Entity) tamer, includeTargets, visitedEntities);
             }
         }
         return null;
+    }
+
+    public static Player getPlayerCauser(Entity causer, boolean includeTargets) {
+        return getPlayerCauser(causer, includeTargets, new HashSet<>());
     }
 
     public static Player getPlayerCauser(Entity causer) {
