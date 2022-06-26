fc6f4b1c8ff3419cfcb87e54383db8e7dd1d935a
froobynooby <froobynooby@froobworld.com>
2022-06-26 10:26:45 +0930

    Send periodic reminders about unread mail

--- src/main/java/com/froobworld/nabsuite/modules/basics/player/mail/MailCentre.java
+++ src/main/java/com/froobworld/nabsuite/modules/basics/player/mail/MailCentre.java
@@ -3,7 +3,6 @@ package com.froobworld.nabsuite.modules.basics.player.mail;
 import com.froobworld.nabsuite.data.DataLoader;
 import com.froobworld.nabsuite.data.DataSaver;
 import com.froobworld.nabsuite.modules.basics.BasicsModule;
-import com.froobworld.nabsuite.modules.basics.player.mail.MailBox;
 import com.froobworld.nabsuite.util.ConsoleUtils;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
@@ -18,15 +17,20 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.io.File;
+import java.util.Map;
 import java.util.UUID;
+import java.util.WeakHashMap;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 public class MailCentre implements Listener {
     private static final Pattern fileNamePattern = Pattern.compile("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\\.json$");
+    private static final long reminderFrequency = TimeUnit.MINUTES.toMillis(30);
     private final BasicsModule basicsModule;
     protected final DataSaver mailBoxSaver;
     private final BiMap<UUID, MailBox> mailBoxMap = HashBiMap.create();
     private final File directory;
+    private final Map<Player, Long> lastReminderMap = new WeakHashMap<>();
 
     public MailCentre(BasicsModule basicsModule) {
         this.basicsModule = basicsModule;
@@ -41,6 +45,7 @@ public class MailCentre implements Listener {
         mailBoxSaver.start();
         mailBoxSaver.addDataType(MailBox.class, mailBox -> mailBox.toJsonString().getBytes(), mailBox -> new File(directory, mailBox.getUuid().toString() + ".json"));
         Bukkit.getPluginManager().registerEvents(this, basicsModule.getPlugin());
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(basicsModule.getPlugin(), this::doReminderCycle, 0, 300);
     }
 
     public void shutdown() {
@@ -69,6 +74,7 @@ public class MailCentre implements Listener {
                     Component.text("You have received some mail (/mail read).", NamedTextColor.YELLOW)
                             .clickEvent(ClickEvent.runCommand("/mail read"))
             );
+            lastReminderMap.put(player, System.currentTimeMillis());
         }
         return mail;
     }
@@ -82,6 +88,21 @@ public class MailCentre implements Listener {
                                 .clickEvent(ClickEvent.runCommand("/mail read"))
                 );
             }, 20);
+            lastReminderMap.put(event.getPlayer(), System.currentTimeMillis());
+        }
+    }
+
+    private void doReminderCycle() {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (getMailBox(player.getUniqueId()).hasUnread()) {
+                if (System.currentTimeMillis() - lastReminderMap.computeIfAbsent(player, p -> System.currentTimeMillis()) >= reminderFrequency) {
+                    player.sendMessage(
+                            Component.text("You have unread mail (/mail read).").color(NamedTextColor.YELLOW)
+                                    .clickEvent(ClickEvent.runCommand("/mail read"))
+                    );
+                    lastReminderMap.put(player, System.currentTimeMillis());
+                }
+            }
         }
     }
 
