e9f2b8ae735f15d3420c0e3620d380a74d93d541
froobynooby <froobynooby@froobworld.com>
2022-07-22 19:12:32 +0930

    Fix End warning message and offset initially by a minute

--- src/main/java/com/froobworld/nabsuite/modules/mechs/end/EndManager.java
+++ src/main/java/com/froobworld/nabsuite/modules/mechs/end/EndManager.java
@@ -10,16 +10,17 @@ import org.bukkit.entity.Player;
 public class EndManager {
     private static final int REMINDER_DISTANCE = 1500;
     private static final int REMINDER_PERIOD = 1200 * 15; // 15 minutes
+    private static final int INITIAL_DELAY = REMINDER_PERIOD + 1200;
 
     public EndManager(MechsModule mechsModule) {
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(mechsModule.getPlugin(), this::sendReminders, REMINDER_PERIOD, REMINDER_PERIOD);
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(mechsModule.getPlugin(), this::sendReminders, REMINDER_PERIOD, INITIAL_DELAY);
     }
 
     private void sendReminders() {
         for (Player player : Bukkit.getOnlinePlayers()) {
             if (player.getWorld().getEnvironment() == World.Environment.THE_END) {
                 if (Math.max(Math.abs(player.getLocation().getBlockX()), Math.abs(player.getLocation().getBlockZ())) > REMINDER_DISTANCE) {
-                    player.sendMessage(Component.text("Please note: the area of the End you are may be reset without notice.", NamedTextColor.RED));
+                    player.sendMessage(Component.text("Please note: the area of the End you are in may be reset without notice.", NamedTextColor.RED));
                     player.sendMessage(Component.text("Do not place anything you are not willing to lose.", NamedTextColor.RED));
                 }
             }
