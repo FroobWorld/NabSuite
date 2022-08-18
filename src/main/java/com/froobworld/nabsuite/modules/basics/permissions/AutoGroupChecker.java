b453201f4e0727b7cba72817ffa29dee423438c3
froobynooby <froobynooby@froobworld.com>
2022-08-18 19:05:42 +0930

    Fix auto promotion for users in non-track groups

--- src/main/java/com/froobworld/nabsuite/modules/basics/permissions/AutoGroupChecker.java
+++ src/main/java/com/froobworld/nabsuite/modules/basics/permissions/AutoGroupChecker.java
@@ -3,10 +3,14 @@ package com.froobworld.nabsuite.modules.basics.permissions;
 import com.froobworld.nabsuite.modules.basics.BasicsModule;
 import com.froobworld.nabsuite.modules.basics.player.PlayerData;
 import net.luckperms.api.LuckPerms;
+import net.luckperms.api.model.group.Group;
 import net.luckperms.api.model.user.User;
+import net.luckperms.api.query.QueryOptions;
 import net.luckperms.api.track.Track;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+
+import java.util.Collection;
 
 public class AutoGroupChecker {
     private final BasicsModule basicsModule;
@@ -34,13 +38,32 @@ public class AutoGroupChecker {
             long timePlayedMillis = System.currentTimeMillis() - playerData.getFirstJoined();
             double daysPlayed = (double) timePlayedMillis / 1000.0 / 60.0 / 60.0 / 24.0;
 
-            int daysRequired = basicsModule.getConfig().autoPromote.requiredTime.of(user.getPrimaryGroup()).get();
+            String trackPosition = getTrackPosition(user, track);
+            if (trackPosition == null) {
+                return;
+            }
+            int daysRequired = basicsModule.getConfig().autoPromote.requiredTime.of(trackPosition).get();
             if (daysRequired > 0 && daysPlayed > daysRequired) {
                 track.promote(user, luckPerms.getContextManager().getStaticContext());
                 luckPerms.getUserManager().saveUser(user);
             }
 
         });
+    }
+
+    private String getTrackPosition(User user, Track track) {
+        Collection<Group> userGroups = user.getInheritedGroups(QueryOptions.defaultContextualOptions());
+        Group highestTrackPosition = null;
+        for (String group : track.getGroups()) {
+            for (Group userGroup : userGroups) {
+                if (group.equalsIgnoreCase(userGroup.getName())) {
+                    if (highestTrackPosition == null || userGroup.getWeight().orElse(Integer.MIN_VALUE) > highestTrackPosition.getWeight().orElse(Integer.MIN_VALUE)) {
+                        highestTrackPosition = userGroup;
+                    }
+                }
+            }
+        }
+        return highestTrackPosition == null ? null : highestTrackPosition.getName();
     }
 
 }
