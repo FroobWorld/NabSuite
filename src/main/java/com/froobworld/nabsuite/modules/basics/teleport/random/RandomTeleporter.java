3be0236f2e1cf91921b6c1ce17bff75dfe321ccb
froobynooby <froobynooby@froobworld.com>
2022-09-19 21:38:26 +0930

    Unbound from below one of the coordinates when finding random teleport locations
    Fixes an issue where random teleports only take users to the corners of the map

--- src/main/java/com/froobworld/nabsuite/modules/basics/teleport/random/RandomTeleporter.java
+++ src/main/java/com/froobworld/nabsuite/modules/basics/teleport/random/RandomTeleporter.java
@@ -52,8 +52,9 @@ public class RandomTeleporter {
 
     private Location generateRandomLocation(World world) {
         BasicsConfig.RandomTeleportSettings.WorldSettings worldSettings = basicsModule.getConfig().randomTeleport.worldSettings.of(world);
-        int offsetX = (random.nextBoolean() ? -1 : 1) * (worldSettings.minorRadiusX.get() + random.nextInt(worldSettings.majorRadiusX.get() - worldSettings.minorRadiusX.get()));
-        int offsetZ = (random.nextBoolean() ? -1 : 1) * (worldSettings.minorRadiusZ.get() + random.nextInt(worldSettings.majorRadiusZ.get() - worldSettings.minorRadiusZ.get()));
+        boolean unboundedX = random.nextBoolean();
+        int offsetX = (random.nextBoolean() ? -1 : 1) * (unboundedX ? random.nextInt(worldSettings.majorRadiusX.get()) : (worldSettings.minorRadiusX.get() + random.nextInt(worldSettings.majorRadiusX.get() - worldSettings.minorRadiusX.get())));
+        int offsetZ = (random.nextBoolean() ? -1 : 1) * (unboundedX ? (worldSettings.minorRadiusZ.get() + random.nextInt(worldSettings.majorRadiusZ.get() - worldSettings.minorRadiusZ.get())) : random.nextInt(worldSettings.majorRadiusZ.get()));
 
         return new Location(world, worldSettings.centreX.get() + offsetX + 0.5, 0, worldSettings.centreZ.get() + offsetZ + 0.5);
     }
