a008942ed8ce8e81e367fcc92f37b3b10d771935
froobynooby <froobynooby@froobworld.com>
2024-06-10 15:53:03 +0930

    Stop explosions breaking armour stands and hanging entities in areas with no-explode

--- src/main/java/com/froobworld/nabsuite/modules/protect/area/flag/enforcers/NoExplodeFlagEnforcer.java
+++ src/main/java/com/froobworld/nabsuite/modules/protect/area/flag/enforcers/NoExplodeFlagEnforcer.java
@@ -4,11 +4,15 @@ import com.froobworld.nabsuite.modules.protect.area.AreaLike;
 import com.froobworld.nabsuite.modules.protect.area.AreaManager;
 import com.froobworld.nabsuite.modules.protect.area.flag.Flags;
 import org.bukkit.Location;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.*;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.hanging.HangingBreakByEntityEvent;
+import org.bukkit.event.hanging.HangingBreakEvent;
 
 public class NoExplodeFlagEnforcer implements Listener {
     private final AreaManager areaManager;
@@ -34,6 +38,29 @@ public class NoExplodeFlagEnforcer implements Listener {
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
     public void onEntityExplode(EntityExplodeEvent event) {
         event.blockList().removeIf(block -> !canExplode(block.getLocation()));
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void onHangingBreak(HangingBreakByEntityEvent event) {
+        if (event.getCause() != HangingBreakEvent.RemoveCause.EXPLOSION) {
+            return;
+        }
+        if (!canExplode(event.getEntity().getLocation())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void onHitArmourStand(EntityDamageEvent event) {
+        if (event.getCause() != EntityDamageEvent.DamageCause.BLOCK_EXPLOSION && event.getCause() != EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) {
+            return;
+        }
+        if (!(event.getEntity() instanceof ArmorStand)) {
+            return;
+        }
+        if (!canExplode(event.getEntity().getLocation())) {
+            event.setCancelled(true);
+        }
     }
 
 }
